725d9f6e4ea17fcca52aab161616da63c186e51a
mim-1999 <tasnimkhondoker1999@gmail.com>
2025-10-21 09:14:12 -0400

    Customer Creation and Updae values

--- customer.py
+++ customer.py
@@ -12,6 +12,6 @@ class Customer():
         self.__account_number = account_number
 
     def createuser(self):
-        temp = db_query(
-            f"INSERT INTO customers VALUES ( '{self.__username}', '{self.__password}', '{self.__name}', '{self.__age}', '{self.__city}', '{self.__account_number}', 1 )")
+        db_query(
+            f"INSERT INTO customers VALUES ( '{self.__username}', '{self.__password}', '{self.__name}', '{self.__age}', '{self.__city}', 0 ,  '{self.__account_number}', 1 )")
         mydb.commit()
